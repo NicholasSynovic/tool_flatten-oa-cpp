2b815c15f8b72b207b821914a787a47180f438db
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-09-19 17:28:16 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include "CLI/CLI.hpp"
-#include <iostream>
+#include <boost/iostreams/stream.hpp>
+#include <filesystem>
+#include <zlib.h>
+
+using namespace std;
 
 int main(int argc, char **argv) {
   CLI::App app;
+
+  string inputPath;
+  app.add_option("-i,--input", inputPath,
+                 "Path to GZip compressed OpenAlex JSON Lines file")
+      ->required();
+
   CLI11_PARSE(app, argc, argv);
 
-  std::cout << "Hello, World!" << std::endl;
+  filesystem::path absInputPath = filesystem::absolute(inputPath);
+
   return 0;
 }
